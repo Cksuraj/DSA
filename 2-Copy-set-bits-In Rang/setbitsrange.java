95ee644c82f0eaa416136413e21959bcbc8aad3f
Cksuraj <sjchaudhary00@gmail.com>
2023-07-02 08:48:00 +0530

    2-july-POTD

---
+++ 2-Copy-set-bits-In Rang/setbitsrange.java
@@ -0,0 +1,35 @@
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line[] = in.readLine().trim().split("\\s+");
+            int x = Integer.parseInt(line[0]);
+            int y = Integer.parseInt(line[1]);
+            int l = Integer.parseInt(line[2]);
+            int r = Integer.parseInt(line[3]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.setSetBit(x, y, l, r));
+        }
+    }
+}
+
+class Solution{
+    static int setSetBit(int x, int y, int l, int r){
+        // code here
+      
+      for(int i=l;i<=r;i++){
+          // if here we shift the 1 bits to words left using Bitwise opratore
+            if((y & (1 << (i-1)) )>0 )
+            // shifting opration
+                x |= (1 << (i-1));
+      }
+      return x;
+    
+    }
+}
