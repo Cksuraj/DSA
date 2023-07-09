463d890f2ba99a7af23bcffec2872b1873bb96b3
Cksuraj <sjchaudhary00@gmail.com>
2023-07-09 19:55:24 +0530

    9-july-POTD

---
+++ 9-Smallest positive missing number/smallmissingno.java
@@ -0,0 +1,24 @@
+
+class Solution
+{
+    //Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int n)
+    {
+        //  Vis array to check is it visited or not 
+        
+        boolean vis[]=new boolean[n+1];
+        
+        for(int i=0;i<n;i ++){
+            if(arr[i] > 0 && arr[i]<=n){
+                vis[arr[i]]=true;
+            }
+        }
+        
+        for(int i=1;i<=n;i++){
+            if(!vis[i])
+            return i;
+        }
+        
+        return n+1;
+    }
+}
