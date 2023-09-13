8063c99a167f81fa755154d911b50b0c2aa6dd9d
Cksuraj <sjchaudhary00@gmail.com>
2023-09-13 22:09:32 +0530

    13-SEP-POTD

---
+++ 69-Larget possible number/largest.java
@@ -0,0 +1,26 @@
+//User function Template for Java
+
+class Solution{
+    static String findLargest(int N, int S){
+        // code here
+        
+        StringBuilder ans=new StringBuilder();
+        if(S==0 && N>1)
+        return "-1";
+        
+        for(int i=0;i<N;i++){
+            if(S>=9){
+            ans.append('9');
+            S-=9;
+            }
+            else {
+                ans.append((char)(S + 48));
+                S=0;
+            }
+        }
+        
+        if(S>0) return "-1";
+        
+        return ans.toString();
+    }
+}
